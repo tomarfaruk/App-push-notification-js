37b2271f0211023e94487741a89d7604295d8a77
OMAR FARUK <umorfarukcse@gmail.com>
2020-09-21 00:43:22 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 
+// module.exports = {
+//     ...require("./sell.js"),
+//     ...require("./buy.js"),
+//     // ...require("./lib/bar.js") // add as many as you like
+//   }; 
+
 
 
 exports.onCreateActivityFeedItem = functions.firestore
